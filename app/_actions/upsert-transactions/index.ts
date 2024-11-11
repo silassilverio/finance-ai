0c30b0199edd24324439f8bb138843b9a7beb617
Silas Silverio <silas.silverio@gorilainvest.com.br>
2024-11-11 17:49:55 -0300

    feat: ensure transaction can be updated

---
+++ app/_actions/upsert-transactions/index.ts
@@ -0,0 +1,38 @@
+"use server";
+
+import { db } from "@/app/_lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import {
+  TransactionCategory,
+  TransactionPaymentMethod,
+  TransactionType,
+} from "@prisma/client";
+import { addTransactionsShecma } from "./schema";
+import { revalidatePath } from "next/cache";
+
+interface addTransactionsParams {
+  id?: string;
+  name: string;
+  amount: number;
+  type: TransactionType;
+  category: TransactionCategory;
+  paymentMethod: TransactionPaymentMethod;
+  date: Date;
+}
+
+export const upsertTransactions = async (params: addTransactionsParams) => {
+  addTransactionsShecma.parse(params);
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+  await db.transaction.upsert({
+    where: {
+      id: params.id,
+    },
+    update: { ...params, userId },
+    create: { ...params, userId },
+  });
+
+  revalidatePath("/transactions");
+};
