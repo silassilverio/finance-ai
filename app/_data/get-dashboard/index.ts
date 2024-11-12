cba564470fab315683c935bd99c9aafe3361f9e2
Silas Silverio <silas.silverio@gorilainvest.com.br>
2024-11-12 18:12:03 -0300

    feat: ensure get dashboard function is protected

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -4,9 +4,15 @@ import {
   TotalExpensePerCategory,
   TransactionsPercentagePerType,
 } from "./types";
+import { auth } from "@clerk/nextjs/server";
 
 export const getDashboard = async (month: string) => {
+  const { userId } = auth();
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
   const where = {
+    userId,
     date: {
       gte: new Date(`2024-${month}-01`),
       lt: new Date(`2024-${month}-31`),
